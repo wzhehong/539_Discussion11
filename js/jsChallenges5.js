112d566d5129a89431fc06da3ed7a9257a1f9760
wzhehong <84788737+wzhehong@users.noreply.github.com>
2023-11-07 20:53:31 -0500

    Add files via upload

--- js/jsChallenges5.js
+++ js/jsChallenges5.js
@@ -1,13 +1,20 @@
-let c5 = document.querySelector('.thumbnail1');
+let img1 = document.querySelector('#img1');
+let img2 = document.querySelector('#img2');
+let img3 = document.querySelector('#img3');
 let element = document.querySelector('#display');
 
 function c5in(){
+    element.innerHTML = img1.alt;
+    element.style.backgroundImage = "url('"+img1.src+"')";
     console.log('in');
 }
 
 function c5out(){
+    element.innerHTML = 'Hover over an image below to display the image and the alt text.';
+    element.style.background = '#6E3DFF';
+    element.style.backgroundImage = url('');
     console.log('out');
 }
 
-c5.addEventListener('mouseover', c5in);
-c5.addEventListener('mouseout', c5out);
+img1.addEventListener('mouseover', c5in);
+img1.addEventListener('mouseout', c5out);
